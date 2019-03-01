1d0b97afa929c0542ecc1277f70901e229e14092
Unknown <hewitt.archie@yandex.ru>
2019-03-01 15:25:43 +0300

    Fixed logger debug issue

--- lib/src/logger.cpp
+++ lib/src/logger.cpp
@@ -31,7 +31,7 @@ auto create_spdlog() {
   dist_sink->add_sink(debug_sink);
 #endif  // _DEBUG && _MSC_VER
 
-  return std::make_shared<spdlog::logger>("console", debug_sink);
+  return std::make_shared<spdlog::logger>("console", dist_sink);
 }
 
 Logger::Logger(const std::string &section) : section_{section} {
